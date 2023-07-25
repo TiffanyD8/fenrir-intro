4b4ae02154bb68d9ae0eab046ec939fa6d7aea7f
tiffanyd8 <tiffanycooper0425@gmail.com>
2023-07-25 08:06:24 -0500

    4.2 Code Updates

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 // Start of Lesson 4.2 the DOM 
 
-let today = new Date();
-let thisYear = today.getFullYear();
-var copyrightSymbol = "\u00A9";
+const today = new Date();
+const thisYear = today.getFullYear();
+const copyrightSymbol = "\u00A9";
 
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
-copyright.innerHTML = 'Tiffany ' + copyrightSymbol + ' ' + thisYear 
-footer.appendChild(copyright)
+    copyright.innerHTML = 'Tiffany ' + copyrightSymbol + ' ' + thisYear; 
+    footer.appendChild(copyright)
 
 // include skills in this section
 var skills = ['IBM Tools', 'JCL', 'Microsoft Suite', 'Eztrieve', 'Geometry', 'Algebra', 'Pre-Calculus', 'Group Travel', 'Insurance Agent'];
@@ -18,7 +18,10 @@ console.log(skillsSection);
 var skillsList = skillsSection.querySelector('ul');
 
 for (let i = 0; i < skills.length; i++) {
-var skill = document.createElement("li");
-skill.innerHTML = skills[i]
-skillsList.appendChild(skill)
+    var skill = document.createElement("li");
+    skill.innerHTML = skills[i];
+    skillsList.appendChild(skill);
 }
+
+
+
